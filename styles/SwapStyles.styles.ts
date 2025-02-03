167b86b61a2b0bdbdf8cea442a816cb228fa2c29
LashaDevLasha <l.goglidze@citadeli.com>
2025-02-03 16:30:54 +0400

    margin top updated for convert page on mobile sizes

--- styles/SwapStyles.styles.ts
+++ styles/SwapStyles.styles.ts
@@ -12,6 +12,10 @@ export const TradeBox = styled.div`
   align-items: center;
   padding: 20px;
   margin-top: 100px;
+
+  @media (max-width: 768px) {
+    margin-top: 40px; 
+  }
 `;
 
 export const SwitchButton = styled.div`
